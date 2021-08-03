cf770b8a3fbc98c4e4de5ef6db7eb75eb16c43a2
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2021-08-03 14:47:00 -0300

    main and home created

---
+++ web-moderno/users/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+<Main icon="home" title="Início"
+    subtitle="Segundo Projeto do capítulo de React.">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="br-0">Ssitema para exemplificar a construção 
+        de um cadastro desenvolvimento em React</p>
+</Main>
