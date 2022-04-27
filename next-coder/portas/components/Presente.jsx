b257108a794e8692524c031788027cb9ae2aec93
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-04-27 16:02:00 -0300

    Presente criado

---
+++ next-coder/portas/components/Presente.jsx
@@ -0,0 +1,12 @@
+import styles from '../src/styles/Presente.module.scss'
+
+export default function Presente() {
+    return (
+        <div className={styles.presente}>
+            <div className={styles.tampa}>Tampa</div>
+            <div className={styles.corpo}>Corpo</div>
+            <div className={styles.laco1}>Corpo</div>
+            <div className={styles.laco2}>Corpo</div>
+        </div>
+    )
+}
