d7917e6fbc8b0fe71abf45f9367631af58737988
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-04-28 15:10:56 -0300

    Porta created

--- next-coder/portas/src/pages/index.jsx
+++ next-coder/portas/src/pages/index.jsx
@@ -1,10 +1,12 @@
+import { Porta } from "../../components/Porta";
 import Presente from "../../components/Presente";
 
 
 export default function Home() {
   return (
     <div>
-      <Presente />
+      {/* <Presente /> */}
+      <Porta />
     </div>
   )
 }
